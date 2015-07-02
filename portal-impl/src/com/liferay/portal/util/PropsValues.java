dfbd32e757cb96d9d5af7a70b42c4f1f7d13f971
matthewkong <matthew.kong@liferay.com>
2015-07-01 18:11:15 -0700

    LPS-56801 Add option to sync LDAP users through uuid

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -888,6 +888,8 @@ public class PropsValues {
 
 	public static final boolean LDAP_IMPORT_USER_PASSWORD_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_PASSWORD_ENABLED));
 
+	public static final boolean LDAP_IMPORT_USER_SYNC_UUID = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_SYNC_UUID));
+
 	public static final int LDAP_PAGE_SIZE = GetterUtil.getInteger(PropsUtil.get(PropsKeys.LDAP_PAGE_SIZE));
 
 	public static final boolean LDAP_PASSWORD_POLICY_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_PASSWORD_POLICY_ENABLED));
